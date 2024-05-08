31efe103daa6b59c3c49b4ed88eeaf0b0fc07540
Tamara Naumović <tamaranaumovic@gmail.com>
2024-05-08 17:22:35 +0200

    Update init.sql plugin changed

--- 6-compose/db/init.sql
+++ 6-compose/db/init.sql
@@ -1,5 +1,5 @@
 SET GLOBAL log_error_verbosity = 1;
-ALTER USER 'novi'@'%' IDENTIFIED WITH mysql_native_password BY 'novi';
+ALTER USER 'novi'@'%' IDENTIFIED WITH caching_sha2_password BY 'novi';
 GRANT ALL PRIVILEGES ON swfavorites.* TO 'novi'@'%';
 FLUSH PRIVILEGES;
 EXIT;
